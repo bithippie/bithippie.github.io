f3ebee74c7a2188b51748542e17481ae6d43e773
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-11-21 17:45:41 -0300

    feat(components): move `Footer` component to sections directory. Also add backgroundColor prop and adjust padding.

---
+++ components/sections/footer.js
@@ -0,0 +1,27 @@
+export default function Footer({ backgroundColor }) {
+  return (
+    <footer className={`bg-${backgroundColor} pb-14`}>
+      <div className="container grid grid-cols-1 md:grid-cols-12 gap-8 p-8">
+        <div className="col-span-7">
+          <h3 className="text-4xl text-moss">BitHippie LLC</h3>
+          <h4 className="text-xl text-white">
+            At the Intersection of Sustainability, Technology, and Wellness
+          </h4>
+        </div>
+        <div className="col-span-5">
+          <h3 className="text-4xl text-moss">Join the Socials!</h3>
+          <p className="text-white">
+            Check out the{" "}
+            <a
+              href="https://www.youtube.com/@BrainwaveExchange"
+              target="_blank"
+            >
+              <span className="text-old-gold">Brainwave Exchange Podcast</span>
+            </a>{" "}
+            where we talk to good humans making their mark on the world.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
