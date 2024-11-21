948f43a960f0c8fee04ddfc9d92e9a981f06c097
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-11-21 17:47:52 -0300

    feat(components): move `HowWeWork` component to sections directory. Also add backgroundColor prop and adjust height and margin-bottom.

---
+++ components/sections/how_we_work.js
@@ -0,0 +1,46 @@
+import { Avatar, Blockquote } from "flowbite-react";
+
+import EngagementAccordion from "@/components/engagement_accordion";
+import ScheduleConsultation from "@/components/schedule_consultation";
+
+export default function HowWeWork({ backgroundColor }) {
+  return (
+    <section
+      id="how-we-work"
+      className={`bg-${backgroundColor} h-[40rem] mb-[-1px]`}
+    >
+      <div className="container grid grid-cols-2 gap-8 py-8">
+        <EngagementAccordion />
+
+        <div>
+          <h2 className="text-5xl mb-8">A tried and true process</h2>
+          <h3 className="text-3xl">
+            Let our clients will be the first to tell you…
+          </h3>
+
+          <Blockquote className="mt-8">
+            <Avatar
+              className="float-right"
+              rounded
+              size="lg"
+              img="/alex.jpeg"
+              alt="profile picture"
+            />
+            “We were able to go from initial product idea to a demo with
+            stakeholders in just a couple of weeks. This enabled rapid
+            iteration, and minimized the chances of building something that
+            nobody would want. ”
+          </Blockquote>
+
+          <figcaption className="my-6 flex items-center space-x-3">
+            <cite>
+              — Alex Greenfield // Chief Scientist & Founder // Fresnel.bio
+            </cite>
+          </figcaption>
+
+          <ScheduleConsultation />
+        </div>
+      </div>
+    </section>
+  );
+}
