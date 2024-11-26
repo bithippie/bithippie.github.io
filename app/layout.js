7ae1378864368b42495ef2095921536a0d5906cc
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-11-26 13:08:36 -0300

    feat(layout): add head and meta tags to html

--- app/layout.js
+++ app/layout.js
@@ -17,6 +17,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </head>
       <body className={`${marcellus.variable} antialiased`}>{children}</body>
     </html>
   );
