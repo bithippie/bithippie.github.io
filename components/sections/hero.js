c9129f1cae1429f995df9fff1e2a538117f92105
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-11-27 18:47:48 -0300

    feat(hero): add `LineDraw` component

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -1,13 +1,20 @@
+"use client";
+
+import LineDraw from "@/components/line_draw";
 import Reveal from "@/components/reveal";
 import ScheduleConsultation from "@/components/schedule_consultation";
 
 export default function Hero() {
   return (
-    <section id="hero" className={`h-[48rem] bg-hero bg-center bg-cover flex`}>
+    <section id="hero" className="h-[48rem] bg-hero bg-center bg-cover flex">
       <div className="container flex flex-col items-center self-center text-center">
         <Reveal id="revealME">
           <h1 className="text-white text-5xl mt-16 md:text-6xl">
-            Helping good humans <span className="text-moss">sustainably</span>{" "}
+            Helping good humans{" "}
+            <span className="relative inline-block">
+              <span className="text-moss">sustainably</span>
+              <LineDraw hasScroll={true} />
+            </span>{" "}
             deliver digital products customers{" "}
             <span className="text-old-gold">love</span>
           </h1>
