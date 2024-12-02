bdf39fbea5532818a9abb878a31808a456aea9ef
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-12-02 16:37:59 -0300

    feat(css): add margin-bottom to divider

--- components/divider.js
+++ components/divider.js
@@ -1,6 +1,6 @@
 export default function Divider({ backgroundColor, curvePosition }) {
   return (
-    <div className={`relative bg-${backgroundColor}`}>
+    <div className={`relative ${curvePosition}-divider bg-${backgroundColor}`}>
       <div
         className={`relative w-full h-10 sm:h-20 bg-${backgroundColor} ${curvePosition}-curve-divider`}
       ></div>
