7f8949f5db5fdc4f93d0479acb934966bda0d7cb
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-12-12 18:06:28 -0300

    feat(engagement_accordion): implement with new accordion component

--- components/engagement_accordion.js
+++ components/engagement_accordion.js
@@ -1,42 +1,29 @@
 "use client";
 
-import { Accordion } from "flowbite-react";
-
-import { styles } from "@/components/faq";
+import { Accordion } from "@/components/accordion";
 
 export default function EngagementAccordion() {
-  return (
-    <div className="container">
-      <Accordion collapseAll theme={styles.root}>
-        <Accordion.Panel>
-          <Accordion.Title theme={styles.title}>Discover</Accordion.Title>
-          <Accordion.Content theme={styles.content}>
-            We take the time to understand you, your business, and your goals.
-          </Accordion.Content>
-        </Accordion.Panel>
-        <Accordion.Panel>
-          <Accordion.Title theme={styles.title}>Design</Accordion.Title>
-          <Accordion.Content theme={styles.content}>
-            We craft a design plan that includes look and feel, the connection
-            points between systems, and what’s needed now, next, then later.
-          </Accordion.Content>
-        </Accordion.Panel>
-        <Accordion.Panel>
-          <Accordion.Title theme={styles.title}>Develop</Accordion.Title>
-          <Accordion.Content theme={styles.content}>
-            Our first-hand experience, complimented by a meticulously chosen
-            blend of tools, allows us to move fast without sacrificing quality.
-          </Accordion.Content>
-        </Accordion.Panel>
-        <Accordion.Panel>
-          <Accordion.Title theme={styles.title}>Maintain</Accordion.Title>
-          <Accordion.Content theme={styles.content}>
-            We set up automatic detection for when issues arise, and automate
-            resolution where possible. This ensures your product runs smoothly,
-            and expectedly for years to come.
-          </Accordion.Content>
-        </Accordion.Panel>
-      </Accordion>
-    </div>
-  );
+  const accordionItems = [
+    {
+      key: 0,
+      title: "Discover",
+      text: "We take the time to understand you, your business, and your goals.",
+    },
+    {
+      key: 1,
+      title: "Design",
+      text: "We craft a design plan that includes look and feel, the connection points between systems, and what’s needed now, next, then later.",
+    },
+    {
+      key: 2,
+      title: "Develop",
+      text: "Our first-hand experience, complimented by a meticulously chosen blend of tools, allows us to move fast without sacrificing quality.",
+    },
+    {
+      key: 3,
+      title: "Mantain",
+      text: "We set up automatic detection for when issues arise, and automate resolution where possible. This ensures your product runs smoothly, and expectedly for years to come.",
+    },
+  ];
+  return <Accordion accordionItems={accordionItems} />;
 }
