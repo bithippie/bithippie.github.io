2f865445699fd69bad970294dbb48fda52581892
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-12-27 15:18:16 -0300

    style(we_work_together): sort imports, remove comment and break lines

--- components/sections/we_work_together.js
+++ components/sections/we_work_together.js
@@ -1,9 +1,8 @@
 "use client";
 
 import Divider from "@/components/divider";
-import ScheduleConsultation from "@/components/schedule_consultation";
-import { motion } from 'framer-motion';
 import RevealOnScroll from "@/components/reveal";
+import ScheduleConsultation from "@/components/schedule_consultation";
 
 export default function WeWorkTogether({ backgroundColor }) {
   return (
@@ -19,79 +18,83 @@ export default function WeWorkTogether({ backgroundColor }) {
         <div className="flex flex-col space-y-4 my-8 ">
           <RevealOnScroll>
             <div className="flex flex-col my-4 text-center items-stretch lg:flex-row lg:space-x-8">
-              <img src="/idea.png" className="rounded-xl m-auto max-w-96 lg:max-w-80" />
+              <img
+                src="/idea.png"
+                className="rounded-xl m-auto max-w-96 lg:max-w-80"
+              />
               <div className="flex flex-col space-y-8 text-center lg:text-left">
-                <h2 className="text-3xl sm:text-4xl text-moss py-6">Rapid Product Development</h2>
+                <h2 className="text-3xl sm:text-4xl text-moss py-6">
+                  Rapid Product Development
+                </h2>
                 <p className="flex text-xl flex-grow ">
-                  Our unique blend of deep techincal experience, stategic partnerships, maticulously chosen tooling,
-                  and AI companions, means our customers see first delivery of an idea in as little as 1&nbsp;month!
-                  <br/><br/>
-                  Timing is critical: Don&apos;t waste time trying to guess what the market wants - build,
-                  test, measure, and iterate!
+                  Our unique blend of deep techincal experience, stategic
+                  partnerships, maticulously chosen tooling, and AI companions,
+                  means our customers see first delivery of an idea in as little
+                  as 1&nbsp;month!
+                  <br />
+                  <br />
+                  Timing is critical: Don&apos;t waste time trying to guess what
+                  the market wants - build, test, measure, and iterate!
                 </p>
               </div>
             </div>
           </RevealOnScroll>
           <RevealOnScroll>
             <div className="flex flex-col my-4 text-center items-stretch lg:flex-row lg:space-x-8">
-              <img src="/science.png" className="rounded-xl m-auto max-w-96 lg:max-w-80" />
+              <img
+                src="/science.png"
+                className="rounded-xl m-auto max-w-96 lg:max-w-80"
+              />
               <div className="flex flex-col space-y-8 text-center lg:text-left">
-                <h2 className="text-3xl sm:text-4xl text-moss py-6">A Tech Backbone that Scales</h2>
+                <h2 className="text-3xl sm:text-4xl text-moss py-6">
+                  A Tech Backbone that Scales
+                </h2>
                 <p className="text-xl text-justify">
-                  Whether you&apos;re a startup founder bootstrapping your business, or
-                  a mature business processing volumes of data in the cloud - we
-                  know how to design systems for where you are today with a growth
-                  plan for how to get where you&apos;re going.
+                  Whether you&apos;re a startup founder bootstrapping your
+                  business, or a mature business processing volumes of data in
+                  the cloud - we know how to design systems for where you are
+                  today with a growth plan for how to get where you&apos;re
+                  going.
                 </p>
                 <div className="flex flex-wrap space-x-8">
-                  <img src="/icons/aws.png" alt="AWS Icon" className="h-10 my-4" />
-                  <img src="/icons/gcp.png" alt="GCP Icon" className="h-8 my-4" />
-                  <img src="/icons/render.png" alt="Render.com Icon" className="h-8 my-4" />
+                  <img
+                    src="/icons/aws.png"
+                    alt="AWS Icon"
+                    className="h-10 my-4"
+                  />
+                  <img
+                    src="/icons/gcp.png"
+                    alt="GCP Icon"
+                    className="h-8 my-4"
+                  />
+                  <img
+                    src="/icons/render.png"
+                    alt="Render.com Icon"
+                    className="h-8 my-4"
+                  />
                 </div>
               </div>
             </div>
           </RevealOnScroll>
           <RevealOnScroll>
             <div className="flex flex-col my-4 text-center items-stretch lg:flex-row lg:space-x-8">
-              <img src="/community.png" className="rounded-xl m-auto max-w-96 lg:max-w-80" />
+              <img
+                src="/community.png"
+                className="rounded-xl m-auto max-w-96 lg:max-w-80"
+              />
               <div className="flex flex-col space-y-8 text-center lg:text-left">
-                <h2 className="text-3xl sm:text-4xl text-moss py-6">On Demand Resource Pool</h2>
+                <h2 className="text-3xl sm:text-4xl text-moss py-6">
+                  On Demand Resource Pool
+                </h2>
                 <p className="flex text-xl flex-grow ">
-                  Why pay for what you don&apos;t need? Our team of Software, Data, and
-                  Cloud Engineers are available when you need for as long as you
-                  need, keeping your operating expenses as low as possible.
+                  Why pay for what you don&apos;t need? Our team of Software,
+                  Data, and Cloud Engineers are available when you need for as
+                  long as you need, keeping your operating expenses as low as
+                  possible.
                 </p>
               </div>
             </div>
           </RevealOnScroll>
-          {/* <RevealOnScroll>
-            <img src="/science.png" className="rounded-xl m-auto" />
-            <h3 className="text-2xl text-moss my-6">
-              
-            </h3>
-            <p className="text-justify">
-              Whether you&apos;re a startup founder bootstrapping your business, or
-              a mature business processing volumes of data in the cloud - we
-              know how to design systems for where you are today with a growth
-              plan for how to get where you&apos;re going.
-            </p>
-            <div className="flex flex-wrap justify-center space-x-4">
-              <img src="/icons/gcp.png" alt="GCP Icon" className="h-8 my-4" />
-              <img src="/icons/aws.png" alt="AWS Icon" className="h-8 my-4" />
-              <img src="/icons/render.png" alt="Render.com Icon" className="h-8 my-4" />
-            </div>
-          </RevealOnScroll>
-          <RevealOnScroll>
-            <img src="/community.png" className="rounded-xl m-auto" />
-            <h3 className="text-2xl text-moss my-6">
-              On Demand Resource Pool
-            </h3>
-            <p className="text-justify">
-              Why pay for what you don&apos;t need? Our team of Software, Data, and
-              Cloud Engineers are available when you need for as long as you
-              need, keeping your operating expenses as low as possible.
-            </p>
-          </RevealOnScroll> */}
         </div>
         <div className="flex flex-col items-center">
           <ScheduleConsultation />
