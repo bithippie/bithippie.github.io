3c9201fe5a51cf37678fc94ef978dd76e7e35f91
Matheus Henrique <matheushenriquedesouzasilva@protonmail.com>
2024-12-29 05:04:06 -0300

    fix(about): adjust section to make it more visible on medium/large screens

--- components/sections/about.js
+++ components/sections/about.js
@@ -11,44 +11,52 @@ export default function About({ backgroundColor }) {
       id="about"
       className={`relative bg-${backgroundColor} pt-10 pb-20 md:pb-28`}
     >
-      <div className="container grid md:grid-cols-12 gap-8 p-8 max-w-screen-xl">
-        <div className="md:col-span-7">
+      <div className="container grid md:grid-cols-12 gap-8 p-8 max-w-screen-xl items-center">
+        <div className="md:col-span-4 overflow-hidden">
           <RevealOnScroll>
-            <img className="w-full h-auto md:pr-12" src="/globe.png" />
+            <img
+              className="w-full h-auto object-cover md:pr-12 md:aspect-auto"
+              src="/globe.png"
+              alt="Globe"
+              style={{ maxHeight: "400px" }}
+            />
           </RevealOnScroll>
         </div>
-        <div className="md:col-span-5 text-justify flex flex-col items-center">
-          <h2 className="text-3xl sm:text-5xl md:text-2xl text-moss mb-12">
-            About Our Company
-          </h2>
-          <p className="mb-6">
+        <div className="md:col-span-8 text-justify flex flex-col items-start space-y-6">
+          <h2 className="text-4xl text-moss mb-8">About Our Company</h2>
+          <p>
             BitHippie was started by Founder and serial entrepreneur, Anthony
             Torres, when he came to the realization that mission and the people
             impacted matter much more than maximizing shareholder value.
           </p>
-          <p className="mb-6">
+          <p>
             The best way to stay true to that ethos is by aligning his efforts
             with the kind of people who care about the Earth and its
             inhabitants.
           </p>
-          <p className="mb-6">
+          <p>
             Rather than do this work alone, Anthony combined his passions for
             making things, personal development, and service into a
             one-of-a-kind community, a force for good.
           </p>
-          <p className="mb-6">
+          <p>
             Together he, and the friends made along the way, work together to
             make real change in the world, hold one another accountable, improve
             and maintain our health, and constantly remind one another that
             “together we go far.”
           </p>
-          <p className="mb-6">
+          <p>
             If you would like to learn more about the community, please reach
             out on LinkedIn and we can see if you’d be a good fit.
           </p>
-          <a href="https://www.linkedin.com/in/bithippie" target="_blank">
-            <motion.button
-              className="
+          <div className="w-full flex text-center justify-center md:justify-start">
+            <a
+              href="https://www.linkedin.com/in/bithippie"
+              className=""
+              target="_blank"
+            >
+              <motion.button
+                className="
                 bg-transparent 
                 text-moss border 
                 border-moss 
@@ -57,12 +65,13 @@ export default function About({ backgroundColor }) {
                 shadow-lg 
                 hover:shadow-xl
               "
-              whileHover={{ scale: 1.025 }}
-              whileTap={{ scale: 0.96 }}
-            >
-              Learn More
-            </motion.button>
-          </a>
+                whileHover={{ scale: 1.025 }}
+                whileTap={{ scale: 0.96 }}
+              >
+                Learn More
+              </motion.button>
+            </a>
+          </div>
         </div>
       </div>
       <Divider backgroundColor="dark-grey" curvePosition="end" />
