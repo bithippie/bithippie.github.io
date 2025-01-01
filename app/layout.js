4de0a67cf7c47a3e9462d31d67b0b96e690dcd9c
Anthony Torres <70786+am17torres@users.noreply.github.com>
2025-01-01 10:30:05 -0500

    feat(metadata): added site.manifest

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>{metadata.title}</title>
-        
+
+        <link rel="manifest" href="/assets/favicon_io/site.manifest" />
         <link rel="shortcut icon" href="/assets/favicon_io/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon_io/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon_io/favicon-32x32.png"/>
