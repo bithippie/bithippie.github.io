b7b13a5f73e764e87a0e082b82f61d0b62637b76
Anthony Torres <70786+am17torres@users.noreply.github.com>
2025-01-01 10:27:17 -0500

    feat(metadata): added favicon pack

--- app/layout.js
+++ app/layout.js
@@ -21,10 +21,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{metadata.title}</title>
+        
+        <link rel="shortcut icon" href="/assets/favicon_io/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon_io/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon_io/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon_io/favicon-16x16.png"/>
 
         {/* HTML Meta Tags */}
-        <title>{metadata.title}</title>
         <meta name="description" content={metadata.description}/>
 
         {/* Facebook Meta Tags */}
@@ -44,7 +48,7 @@ export default function RootLayout({ children }) {
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      
+
       <body className={`${marcellus.variable} antialiased `}>
         {children}
       </body>
