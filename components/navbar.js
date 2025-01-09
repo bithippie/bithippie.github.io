a3a50e9b764de366749e3219d37db637004ab563
Matheus Henrique <42981857+matheushss1@users.noreply.github.com>
2025-01-09 16:10:39 -0300

    Fix Navbar Duplicate Display on Window Resize (#35)
    * style(navbar): sort imports
    * feat(navbar): add event listener for closing the nav when resizing and innerWidth is gte than 768
    * feat(utils): add `tailwindScreens` helper module
    * feat(navbar): adjust hardcoded value to be retrieved from tailwind configs instead
    * fix(navbar): explicitly render specific navbars conditionally
    * feat(navbar): add state to detect small screens
    Introduced `isSmallScreen` state and screen size calculation logic
    using Tailwind's `screens.md` to dynamically identify small screens.
    This improves responsiveness and simplifies conditional rendering.
    * refactor(navbar): encapsulate resize logic in a single function
    Updated `handleResize` to consistently check if `window` is defined
    and use the parsed screen size. Added dependency `mdScreen` to the
    `useEffect` to ensure correct recalculation when screen thresholds change.
    * feat(navbar): add priority flag to logo image
    * fix(navbar): adjust small-screen menu rendering
    Wrapped small-screen menu behavior in an additional conditional
    to ensure the hamburger button and motion animations are only
    visible when applicable. Removed direct `window.innerWidth` checks
    from JSX in favor of the `isSmallScreen` state.
    * refactor(navbar): simplify styles and improve layout structure
    - Removed unused styles for responsive behavior.
    - Simplified `div` and `ul` classes for better readability and maintainability.
    - Ensured consistent styling across navigation links by removing redundant `md:*` class prefixes.
    This change improves code clarity and reduces unnecessary complexity.

--- components/navbar.js
+++ components/navbar.js
@@ -1,12 +1,31 @@
 "use client";
 
-import { useState } from "react";
-import Image from 'next/image'
-import Link from "next/link";
+import { useEffect, useState } from "react";
+
 import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+
+import screens from "@/utils/tailwindScreens";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const mdScreen = parseInt(screens.md);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (typeof window !== "undefined") {
+        setIsSmallScreen(window.innerWidth < mdScreen);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [mdScreen]);
 
   return (
     <section className="w-full">
@@ -14,118 +33,125 @@ export default function NavBar() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between container">
           <Link href="/">
             <Image
-              src="/assets/images/logo.png" 
-              className="h-20" 
+              src="/assets/images/logo.png"
+              className="h-20"
               alt="BitHippie Logo"
               width={300}
-              height={125}/>
+              height={125}
+              priority
+            />
           </Link>
 
-          <button
-            onClick={() => setIsOpen(!isOpen)}
-            type="button"
-            className="inline-flex items-center rounded-lg bg-transparent p-2 text-sm text-gray-500 focus:outline-none md:hidden"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="#AABD7B"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <AnimatePresence>
-            {isOpen && (
-              <motion.div
-                initial={{ opacity: 0, height: 0 }}
-                animate={{ opacity: 1, height: "auto" }}
-                exit={{ opacity: 0, height: 0 }}
-                transition={{ duration: 0.5 }}
-                className="w-full bg-gradient-to-b from-transparent to-bg-dark-grey backdrop-blur-md opacity-95"
+          {isSmallScreen ? (
+            <>
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="inline-flex items-center rounded-lg bg-transparent p-2 text-sm text-gray-500 focus:outline-none md:hidden"
+                aria-expanded="false"
               >
-                <ul className="mt-4 flex flex-col">
-                  <li>
-                    <Link
-                      className="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 text-platinum text-2xl hover:text-moss"
-                      href="#services"
-                    >
-                      Services
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 text-platinum text-2xl hover:text-moss"
-                      href="#faq"
-                    >
-                      FAQ
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 text-platinum text-2xl hover:text-moss"
-                      href="#schedule"
-                    >
-                      Schedule
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="block py-2 pl-3 pr-4 md:p-0 border-b border-gray-100 hover:bg-gray-50 text-platinum text-2xl hover:text-moss"
-                      href="#about"
-                    >
-                      About
-                    </Link>
-                  </li>
-                </ul>
-              </motion.div>
-            )}
-          </AnimatePresence>
-          <div className="w-full hidden md:block md:w-auto bg-gradient-to-b from-transparent to-bg-dark-grey backdrop-blur-md opacity-95">
-            <ul className="mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium">
-              <li>
-                <Link
-                  className="block py-2 pl-3 pr-4 md:p-0 border-b border-gray-100 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white text-platinum text-2xl hover:text-moss"
-                  href="#services"
+                <span className="sr-only">Open main menu</span>
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 17 14"
                 >
-                  Services
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="block py-2 pl-3 pr-4 md:p-0 border-b border-gray-100 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white text-platinum text-2xl hover:text-moss"
-                  href="#faq"
-                >
-                  FAQ
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="block py-2 pl-3 pr-4 md:p-0 border-b border-gray-100 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white text-platinum text-2xl hover:text-moss"
-                  href="#schedule"
-                >
-                  Schedule
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="block py-2 pl-3 pr-4 md:p-0 border-b border-gray-100 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-cyan-700 md:dark:hover:bg-transparent md:dark:hover:text-white text-platinum text-2xl hover:text-moss"
-                  href="#about"
-                >
-                  About
-                </Link>
-              </li>
-            </ul>
-          </div>
+                  <path
+                    stroke="#AABD7B"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 1h15M1 7h15M1 13h15"
+                  />
+                </svg>
+              </button>
+              <AnimatePresence>
+                {isOpen && (
+                  <motion.div
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: "auto" }}
+                    exit={{ opacity: 0, height: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className="w-full bg-gradient-to-b from-transparent to-bg-dark-grey backdrop-blur-md opacity-95"
+                  >
+                    <ul className="mt-4 flex flex-col">
+                      <li>
+                        <Link
+                          className="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 text-platinum text-2xl hover:text-moss"
+                          href="#services"
+                        >
+                          Services
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          className="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 text-platinum text-2xl hover:text-moss"
+                          href="#faq"
+                        >
+                          FAQ
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          className="block py-2 pl-3 pr-4 border-b border-gray-100 hover:bg-gray-50 text-platinum text-2xl hover:text-moss"
+                          href="#schedule"
+                        >
+                          Schedule
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          className="block py-2 pl-3 pr-4 md:p-0 border-b border-gray-100 hover:bg-gray-50 text-platinum text-2xl hover:text-moss"
+                          href="#about"
+                        >
+                          About
+                        </Link>
+                      </li>
+                    </ul>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </>
+          ) : (
+            <div className="w-auto">
+              <ul className="flex mt-0 flex-row space-x-8 text-sm font-medium">
+                <li>
+                  <Link
+                    className="block p-0 border-gray-100 hover:bg-gray-50 border-0 hover:bg-transparent hover:text-cyan-700 text-platinum text-2xl hover:text-moss"
+                    href="#services"
+                  >
+                    Services
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="block p-0 border-gray-100 hover:bg-gray-50 border-0 hover:bg-transparent hover:text-cyan-700 text-platinum text-2xl hover:text-moss"
+                    href="#faq"
+                  >
+                    FAQ
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="block p-0 border-gray-100 hover:bg-gray-50 border-0 hover:bg-transparent hover:text-cyan-700 text-platinum text-2xl hover:text-moss"
+                    href="#schedule"
+                  >
+                    Schedule
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="block p-0 border-gray-100 hover:bg-gray-50 border-0 hover:bg-transparent hover:text-cyan-700 text-platinum text-2xl hover:text-moss"
+                    href="#about"
+                  >
+                    About
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </nav>
     </section>
